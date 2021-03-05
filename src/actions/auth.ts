be67097922d6d144df2d1936fe73e73e850abf44
Gromov Andrey <gromovandrey1985@gmail.com>
2021-03-05 19:33:15 +0300

    Add files via upload
    auth action update

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,11 +1,25 @@
-import { LOG_IN } from '../constants'
-import { IAuthAction } from '../types'
+import { LOG_IN, LOG_OUT, IS_AUTH } from '../constants'
+import { IAuthAction, IAuthActionLogOut, IIsAuthAction } from '../types'
 
 export default {
-    auth(login: string, password: string): IAuthAction {
+    isAuth(): IIsAuthAction {
+        const getStorage = localStorage.getItem('isAuth')
+        return {
+            type: IS_AUTH,
+            payload: Boolean(getStorage)
+        }
+    },
+
+    authLogin(login: string, password: string): IAuthAction {
         return {
             type: LOG_IN,
             payload: { login, password }
+        }
+    },
+
+    authLogout(): IAuthActionLogOut {
+        return {
+            type: LOG_OUT
         }
     }
 }
