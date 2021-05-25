32fe1c96840882a126de5b819a99c57fd9ada162
Александр Долгов <ai.dolgov@i-teco.ru>
2021-05-25 14:58:06 +0300

    init project

--- src/actions/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AppActions } from './app'
-export { default as CartPageActions } from './cartPage'
-export { default as FavoritesPageActions } from './favorites'
-export { default as AuthPageActions } from './auth'
--- src/pages/error/Error.tsx
+++
@@ -1,5 +0,0 @@
-import React  from 'react';
-
-export const Error = () => (
-    <div>Что то пошло не так =/</div>
-);  
